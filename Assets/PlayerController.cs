5c0ae2553b0b63c424740c3ee43fce5ce8e2772e
dz1mka <dzimka@gmail.com>
2025-02-17 18:34:34 +0200

    2 -Casting a Spell

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -17,12 +17,42 @@ public class PlayerController : MonoBehaviour
         anim.SetBool("isJumping", false);
     }
 
+    void StopMagic()
+    {
+        anim.SetBool("isMagic", false);
+    }
+
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Space))
         {
             anim.SetBool("isJumping", true);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.M))
+        {
+            anim.SetBool("isMagic", true);
+        }
+
+        else if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            this.transform.Rotate(Vector3.up * 90);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            this.transform.Rotate(Vector3.up * -90);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            this.transform.Translate(-0.1f, 0, 0);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            this.transform.Translate(0.1f, 0, 0);
         }
     }
 }
